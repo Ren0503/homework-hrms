a21ac323c8edb380871cf763102615a679484c0f
Ren <54244073+Ren0503@users.noreply.github.com>
2022-07-02 10:13:37 +0700

    Update logging.js

--- src/config/logging.js
+++ src/config/logging.js
@@ -65,7 +65,12 @@ const logger = winston.createLogger({
         }),
         esTransport,
     ],
-    format: ecsFormat(),
+      json: false,
+      format: ecsFormat(),
+      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+      msg: 'HTTP {{req.method}} {{req.url}} {{req.body}} {{res.responseTime}}ms', // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+      expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+      colorize: false,
 })
 
 module.exports = logger
