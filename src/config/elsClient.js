14e934ad09983f2fc78b2a88e309063be2a6e8e0
Ren <54244073+Ren0503@users.noreply.github.com>
2022-07-03 14:29:08 +0700

    Update elsClient.js

--- src/config/elsClient.js
+++ src/config/elsClient.js
@@ -5,23 +5,6 @@ const {
 } = require('winston-elasticsearch')
 // Add this to the VERY top of the first file loaded in your app
 
-const apm = require('elastic-apm-node').start({
-    // Override the service name from package.json
-    // Allowed characters: a-z, A-Z, 0-9, -, _, and space
-    serviceName: 'ENVIRONMENT_ALL',
-
-    // Use if APM Server requires a secret token
-    secretToken: 'em9vdpOruURTPaZJ9t',
-
-    // Set the custom APM Server URL (default: http://localhost:8200)
-    serverUrl: 'https://f0ef2851a0c84afe86f5ebc25483131c.apm.us-central1.gcp.cloud.es.io:443',
-
-    // Set the service environment
-    environment: 'all'
-})
-const err = new Error('Ups, something broke!')
-
-apm.captureError(err)
 const client = new Client({
     cloud: {
         id: "MyDev:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvOjQ0MyQ2NTAxOGNkNTEzZGE0MDAwOWExYzM4ZTQ3ODQ2ZGIyZCRkZWU1ODU1NjE1NzE0YjcwODU2NzU5ZGYwYmExYzE5Nw==",
