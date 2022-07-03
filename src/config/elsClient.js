8a56daa3bee692d3a2a5be15763f6a3f4ffa2f49
Ren <54244073+Ren0503@users.noreply.github.com>
2022-07-03 21:15:23 +0700

    Update elsClient.js

--- src/config/elsClient.js
+++ src/config/elsClient.js
@@ -19,7 +19,25 @@ client.info()
     .then((response) => console.log(response))
     .catch((error) => console.log(error))
 
+const apm = require('elastic-apm-node').start({
+    // Override the service name from package.json
+    // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+    serviceName: 'ENVIRONMENT_ALL',
+
+    // Use if APM Server requires a secret token
+    secretToken: 'em9vdpOruURTPaZJ9t',
+    
+    apiKey: "U21rZXc0RUJ4UTBaSndEVGhqb2M6alVCV09nMWRUOVdWbnUzNi0wVm1SUQ==",
+
+    // Set the custom APM Server URL (default: http://localhost:8200)
+    serverUrl: 'https://f0ef2851a0c84afe86f5ebc25483131c.apm.us-central1.gcp.cloud.es.io:443',
+
+    // Set the service environment
+    environment: 'all'
+})
+
 const esTransportOpts = {
+    apm,
     level: 'info',
     client,
     transformer: (logData) => {
