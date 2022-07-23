c3315a0431104ecee3d3e0ae11d440e4084dff2b
Ren <54244073+Ren0503@users.noreply.github.com>
2022-07-23 21:18:14 +0700

    Update logging.js

--- src/config/logging.js
+++ src/config/logging.js
@@ -1,76 +1,54 @@
 const winston = require('winston')
 const { ElasticsearchTransport } = require('winston-elasticsearch');
 const ecsFormat = require('@elastic/ecs-winston-format')
-const { Client } = require('@elastic/elasticsearch')
+const client = require('./elsClient')
 
-const client = new Client({
-    cloud: {
-        id: "MyDev:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvOjQ0MyQ2NTAxOGNkNTEzZGE0MDAwOWExYzM4ZTQ3ODQ2ZGIyZCRkZWU1ODU1NjE1NzE0YjcwODU2NzU5ZGYwYmExYzE5Nw==",
-    },
-    auth: {
-        username: "elastic",
-        password: "djiwSrbnSrSVRqSSOgS6WPJJ",
-    }
-})
-
-client.info()
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error))
-
-const esTransportOpts = {
-    level: 'info',
-    client,
-};
-const esTransport = new ElasticsearchTransport(esTransportOpts);
-esTransport.on('error', (error) => {
-  console.error('Error in logger caught', error);
-});
-const fs = require('fs')
 const logDirectory = 'logs'
-const { combine, timestamp, json } = winston.format
-
 // Create the log directory if it does not exist
 if (!fs.existsSync(logDirectory)) {
     fs.mkdirSync(logDirectory)
 }
 
-const errorFilter = winston.format((info, opts) => {
-    return info.level === 'error' ? info : false
-})
+const name = 'Backend'
 
-const infoFilter = winston.format((info, opts) => {
-    return info.level === 'info' ? info : false
-})
+const fileTransport = (level) => {
+     const fileRotateTransport = new winston.transports.DailyRotateFile({
+      filename: path.join(logDir, /${level}-%DATE%.log),
+      datePattern: 'YYYY-MM-DD',
+      maxSize: '20m',
+      level,
+    })
 
-/** Logging Configurations */
+    return fileRotateTransport
+}
+
+const elasticTransport = (level) => {
+    const esTransport = new ElasticsearchTransport({
+      client,
+      level,
+      indexPrefix: NODE_ENV || 'logs',
+      indexSuffixPattern: 'YYYY-MM-DD',
+      source: name,
+    })
+
+    return esTransport
+}
+
+const level = ['info', 'warn', 'error', 'debug']
+const transports=[]
+for (const l of level) {
+    transports.push(fileTransport(l))
+    transports.push(elasticTransport(l))
+}
+
 const logger = winston.createLogger({
-    level: process.env.LOG_LEVEL || 'info',
-    format: combine(timestamp(), json()),
-    transports: [
-        new winston.transports.File({
-            filename: `${logDirectory}/combined.log`,
-            maxsize: 5242880,
-        }),
-        new winston.transports.File({
-            filename: `${logDirectory}/app-error.log`,
-            level: 'error',
-            format: combine(errorFilter(), timestamp(), json()),
-            maxsize: 5242880,
-        }),
-        new winston.transports.File({
-            filename: `${logDirectory}/app-info.log`,
-            level: 'info',
-            format: combine(infoFilter(), timestamp(), json()),
-            maxsize: 5242880,
-        }),
-        esTransport,
-    ],
-      json: false,
-      format: ecsFormat(),
-      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-      msg: 'HTTP {{req.method}} {{req.url}} {{req.body}} {{res.responseTime}}ms', // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-      expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-      colorize: false,
+    transports,
+    json: false,
+    format: ecsFormat(),
+    meta: true,
+    msg: 'HTTP {{req.method}} {{req.url}} {{req.body}} {{res.responseTime}}ms',
+    expressFormat: true, 
+    colorize: false,
 })
 
 module.exports = logger
